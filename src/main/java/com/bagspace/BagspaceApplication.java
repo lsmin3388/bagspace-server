4c26fc1c3d03fcf7ccbd0304e674796e2867ff48
lsmin3388 <lsmin3388_@naver.com>
2025-03-14 15:33:01 +0900

    init: SpringBoot 3.4.3 개발 환경 추가

---
+++ src/main/java/com/bagspace/BagspaceApplication.java
@@ -0,0 +1,13 @@
+package com.bagspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagspace/BagspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
